4f4d0a9360bedf2a1d0e5410b2372c8f6c32be76
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-07-25 18:12:29 +0500

    start()

--- CRM/src/com/itsuhbat/CRM/service/SellerService.java
+++ CRM/src/com/itsuhbat/CRM/service/SellerService.java
@@ -1,4 +1,46 @@
 package com.itsuhbat.CRM.service;
 
+import com.itsuhbat.CRM.markets.Market;
+
+import static com.itsuhbat.CRM.helper.Config.scanner;
+
 public class SellerService {
+    private Market market;
+
+    public SellerService(Market market) {
+        this.market = market;
+    }
+    public SellerService() {
+
+    }
+
+    public void start(){
+        int action;
+
+        do {
+            System.out.println(
+                    "Menyuni tanlang: " +
+                            "\n[1].Mahsulot qo'shish " +
+                            "\n[2].Mahsulotlar ro'yxati " +
+                            "\n[3].Mahsulotlar ketma-ketlikda chiqarish " +
+                            "\n[4].Mahsulotni o'chirish " +
+                            "\n[5].Market ma'lumotlari " +
+                            "\n[6].Market ma'lumotlarini o'zgartirish " +
+                            "\n[0].Dasturni tugatish"+"\n");
+            action = scanner.nextInt();
+            switch (action){
+                case 1: market.addProduct(); break;
+                case 2: market.printProduct(); break;
+                case 3: {
+                    System.out.println("o'lchamini kiriting: ");
+                    int size = scanner.nextInt();
+                    market.printProduct(size); break;
+                }
+                case 4: market.deleteProduct(); break;
+                case 5: market.marketInfo(); break;
+                case 6: market.changeMarketInfo(); break;
+                case 0: System.exit(0);
+            }
+        }while (true);
+    }
 }
