37e9ba33d9df01f9301bd4df80bfbed846bf8b10
sardoruktamov <73293096+sardoruktamov@users.noreply.github.com>
2022-07-25 14:29:18 +0500

    getter and setter add

--- CRM/src/com/itsuhbat/CRM/personnel/User.java
+++ CRM/src/com/itsuhbat/CRM/personnel/User.java
@@ -6,5 +6,38 @@ public class User {
     String lastName;
     Double account;
 
+    public User(String firstName, String lastName, Double account) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.account = account;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Double getAccount() {
+        return account;
+    }
+
+    public void setAccount(Double account) {
+        this.account = account;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Ism: %s\n Familya: %s \nXisobRaqam: %f", firstName, lastName, account);
+    }
 }
