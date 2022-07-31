2f925364293c3d433b0358c08c363268ccf42fb7
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-07-31 18:21:56 +0500

    add BuyerServiceImple implements

--- CRM/src/com/itsuhbat/CRM/service/BuyerService.java
+++ CRM/src/com/itsuhbat/CRM/service/BuyerService.java
@@ -1,99 +1,14 @@
 package com.itsuhbat.CRM.service;
 
-import com.itsuhbat.CRM.markets.Market;
-import com.itsuhbat.CRM.personnel.User;
+public interface BuyerService {
 
-import static com.itsuhbat.CRM.helper.Config.scanner;
+    //xaridorga aloqador ma`lumotlarni tanlash menyusi
+    void start();
 
-public class BuyerService {
-    private Market market;
-    private User user;
+    // xaridor ma'lumotlarini o`zgartirish
+    void changeUserInfo();
+    //xaridor ma`lumotini kiritish
+    void initUser();
 
-    public BuyerService(Market market) {
-        this.market = market;
-    }
-
-    public void start(){
-        initUser();
-
-        int action;
-
-        while (true){
-            System.out.println(
-                    "Menyuni tanlang: " +
-                            "\n[1].Shaxsiy ma'lumotlar " +
-                            "\n[2].Shaxsiy ma'lumotlarni o'zgartirish " +
-                            "\n[3].Market ma'lumotlari " +
-                            "\n[4].Mahsulotlar ro'yxati " +
-                            "\n[5].Mahsulotlar ketma-ketlikda chiqarish " +
-                            "\n[6].Mahsulot sotib olish " +
-                            "\n[7].Hisobni toldirish " +
-                            "\n[0].Dasturni tugatish");
-            action = scanner.nextInt();
-            switch (action){
-                case 1:
-                    System.out.println(user); break;
-                case 2: changeUserInfo(); break;
-                case 3: market.marketInfo(); break;
-                case 4: market.printProduct(); break;
-                case 5: {
-                    System.out.println("o'lchamini kiriting: ");
-                    int size = scanner.nextInt();
-                    market.printProduct(size); break;
-                }
-                case 6: {
-                    System.out.println("Mahsulot nomerini kiriting: ");
-                    int id = scanner.nextInt();
-                    market.productId(id); break;
-                }
-                case 7:
-                    System.out.println("Pul miqdorini kiriting: ");
-                    Double account = scanner.nextDouble();
-                    user.setAddaccount(account);break;
-                case 0: System.exit(0);
-            }
-        }
-    }
-
-    public void changeUserInfo(){
-        sikl:
-        while (true){
-            System.out.println(user);
-            System.out.println("Qaysi ma'lumotni o'zgartirasiz? ");
-            int result = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (result){
-                case 1:
-                    System.out.println("Ismi qayta kiriting: ");
-                    String firstName = scanner.nextLine();
-                    user.setFirstName(firstName);
-                    break;
-                case 2:
-                    System.out.println("Familyangizni qayta kiriting: ");
-                    String lastName = scanner.nextLine();
-                    user.setLastName(lastName);
-                    break;
-                case 3:
-                    System.out.println("Pul miqdorini qayta kiriting: ");
-                    Double account = scanner.nextDouble();
-                    user.setAccount(account);
-                    break;
-                case 0: break sikl;
-                default:
-                    System.out.println("Noto'g'ri ma'lumot kiritdingiz! \n");break;
-            }
-        }
-    }
-
-    public void initUser(){
-        System.out.println("Ismingizni kiriting: ");
-        scanner.nextLine();
-        String firstName = scanner.nextLine();
-        System.out.println("Familyangizni kiriting: ");
-        String lastName = scanner.nextLine();
-        System.out.println("Xisob raqamdagi pulni kiriting: ");
-        Double account = scanner.nextDouble();
-        this.user = new User(firstName,lastName, account);
-    }
-}
+    void fillAccount();
+};
---
+++ CRM/src/com/itsuhbat/CRM/service/impl/BuyerServiceImple.java
@@ -0,0 +1,186 @@
+package com.itsuhbat.CRM.service.impl;
+
+import com.itsuhbat.CRM.markets.Market;
+import com.itsuhbat.CRM.personnel.User;
+import com.itsuhbat.CRM.products.Product;
+import com.itsuhbat.CRM.service.BuyerService;
+
+import java.util.Scanner;
+
+import static com.itsuhbat.CRM.helper.Config.scanner;
+
+public class BuyerServiceImple implements BuyerService {
+    private Market market;
+    private User user;
+
+    public BuyerServiceImple(Market market) {
+        this.market = market;
+    }
+
+    public void start(){
+        initUser();
+
+        int action;
+
+        while (true){
+            System.out.println(
+                    "Menyuni tanlang: " +
+                            "\n[1].Shaxsiy ma'lumotlar " +
+                            "\n[2].Shaxsiy ma'lumotlarni o'zgartirish " +
+                            "\n[3].Market ma'lumotlari " +
+                            "\n[4].Mahsulotlar ro'yxati " +
+                            "\n[5].Mahsulotlar ketma-ketlikda chiqarish " +
+                            "\n[6].Mahsulot sotib olish " +
+                            "\n[7].Hisobni toldirish " +
+                            "\n[0].Dasturni tugatish");
+            action = scanner.nextInt();
+            switch (action){
+                case 1:
+                    System.out.println(user); break;
+                case 2: changeUserInfo(); break;
+                case 3: market.marketInfo(); break;
+                case 4: market.printProduct(); break;
+                case 5: {
+                    System.out.println("o'lchamini kiriting: ");
+                    int size = scanner.nextInt();
+                    market.printProduct(size); break;
+                }
+                case 6: {
+                    System.out.println("Mahsulot nomerini kiriting: ");
+                    int id = scanner.nextInt();
+                    productId(id); break;
+                }
+                case 7:
+                    fillAccount();break;
+                case 0: System.exit(0);
+            }
+        }
+    }
+
+    public void changeUserInfo(){
+        sikl:
+        while (true){
+            System.out.println(user);
+            System.out.println("Qaysi ma'lumotni o'zgartirasiz? ");
+            int result = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (result){
+                case 1:
+                    System.out.println("Ismi qayta kiriting: ");
+                    String firstName = scanner.nextLine();
+                    user.setFirstName(firstName);
+                    break;
+                case 2:
+                    System.out.println("Familyangizni qayta kiriting: ");
+                    String lastName = scanner.nextLine();
+                    user.setLastName(lastName);
+                    break;
+                case 3:
+                    System.out.println("Pul miqdorini qayta kiriting: ");
+                    Double account = scanner.nextDouble();
+                    user.setAccount(account);
+                    break;
+                case 0: break sikl;
+                default:
+                    System.out.println("Noto'g'ri ma'lumot kiritdingiz! \n");break;
+            }
+        }
+    }
+
+    public void initUser(){
+        System.out.println("Ismingizni kiriting: ");
+        scanner.nextLine();
+        String firstName = scanner.nextLine();
+        System.out.println("Familyangizni kiriting: ");
+        String lastName = scanner.nextLine();
+        System.out.println("Xisob raqamdagi pulni kiriting: ");
+        Double account = scanner.nextDouble();
+        this.user = new User(firstName,lastName, account);
+    }
+
+    public void productId(int id){
+
+        Product[] products = market.getProducts();
+        Product product = null;
+        for (int i = 0; i< products.length; i++){
+            if (id == i+1) {
+                product = products[i];
+                break;
+//                System.out.printf("Omborda %.1f kg %s qolgan \n", products[i-1].getAmount(), products[i-1].getName());
+//                Scanner scanner = new Scanner(System.in);
+//                System.out.printf("Qancha miqdorda %s sotib olmoqchisiz? \n", products[i-1].getName());
+//                double amountProduct = scanner.nextDouble();
+//                if (products[i-1].getAmount() < amountProduct){
+//                    System.out.printf(
+//                            "Omborda %.1f kg %s qolgan, kamroq miqdor kiriting! \n",
+//                            products[i-1].getAmount(), products[i-1].getName()
+//                    );
+//                    productId(id);
+//                }else {
+//                    System.out.println("FIO:  ");
+//                    System.out.println("pul miqdoriiiii:  " + user.getAccount());
+//                    System.out.printf("Siz %.1f so'mlik %s sotib oldingiz! \n", amountProduct*products[i-1].getPrice(), products[i-1].getName());
+//                    System.out.printf("Omborda %.1f kg %s qoldi! \n", products[i-1].getAmount()-amountProduct, products[i-1].getName());
+//                    System.err.println("**************************************");
+//
+//                }
+            }
+
+        }
+        if (product != null){
+            System.out.println(product);
+            System.out.println("mahsulot miqdorini kiriting?  ");
+            double amountProduct = scanner.nextDouble();
+            if (product.getAmount() < amountProduct){
+                    System.out.printf(
+                            "Omborda %.1f kg %s qolgan holos, barchasini sotib olasizmi. ha/yoq ? \n",
+                            product.getAmount(), product.getName()
+                    );
+                    scanner.nextLine();
+                    String res = scanner.nextLine();
+                    if (res.equalsIgnoreCase("ha")){
+                        if (user.getAccount() >= amountProduct*product.getPrice()){
+                            user.setAccount(user.getAccount()-amountProduct*product.getPrice());
+                            System.out.printf("Siz %.1f so'mlik %s sotib oldingiz! \n", amountProduct*product.getPrice(), product.getName());
+                            product.setAmount(0d);
+                            System.out.printf("Omborda %.1f kg %s qoldi! \n", product.getAmount(), product.getName());
+                        }else System.out.println("Xisobingizdagi mablag; yetarli emas! ");
+
+                    }
+                }
+            else {
+                    System.out.printf(
+                                "Omborda %.1f kg %s qolgan, haqiqatddan sotib olmoqchimisiz?  ha/yoq?\n",
+                                product.getAmount(), product.getName()
+                    );
+                    scanner.nextLine();
+                    String res = scanner.nextLine();
+                    if (res.equalsIgnoreCase("ha")){
+                        if (user.getAccount() >= amountProduct*product.getPrice()){
+                            user.setAccount(user.getAccount()-amountProduct*product.getPrice());
+                            System.out.printf("Siz %.1f so'mlik %s sotib oldingiz! \n", amountProduct*product.getPrice(), product.getName());
+                            product.setAmount(product.getAmount()-amountProduct);
+                            System.out.printf("Omborda %.1f kg %s qoldi! \n", product.getAmount(), product.getName());
+                        }else System.out.println("Xisobingizdagi mablag; yetarli emas! ");
+
+                    }else System.out.println("Sotib olishni bekor qildingiz!");
+
+//                    System.out.printf("Siz %.1f so'mlik %s sotib oldingiz! \n", amountProduct*products[i-1].getPrice(), products[i-1].getName());
+//                    System.out.printf("Omborda %.1f kg %s qoldi! \n", products[i-1].getAmount()-amountProduct, products[i-1].getName());
+//                    System.err.println("**************************************");
+
+                }
+        }
+        else System.out.println("Buday ID Maxsulot mavjud emas! ");
+
+    }
+
+    @Override
+    public void fillAccount() {
+        System.out.println("Qo'shmoqchi bo`lgan summagizni kiriting:");
+        double sum = scanner.nextDouble();
+        user.setAccount(user.getAccount()+sum);
+        System.out.println("Balansizgiz to'ldirildi");
+    }
+}
