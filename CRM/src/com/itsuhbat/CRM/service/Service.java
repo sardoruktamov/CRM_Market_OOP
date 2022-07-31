f939e7571b6797c5421052dfa1b10b125a93cfa6
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2022-07-31 18:21:09 +0500

    ADD ServiceImple implements

--- CRM/src/com/itsuhbat/CRM/service/Service.java
+++ CRM/src/com/itsuhbat/CRM/service/Service.java
@@ -1,51 +1,29 @@
 package com.itsuhbat.CRM.service;
 
-import com.itsuhbat.CRM.markets.Market;
-import com.itsuhbat.CRM.products.Product;
+public interface Service {
+    /**
+     * final - xirgi, yakuniy qiyman
+     * final kalit so'zini qo'llanilishi mumkin bolganlar:
+     * field - o'zgaruvchiga yangi qiymat yuklab bo'lmaydi
+     * method - override qilib bo'lmaydi
+     * class - voris olib bo`lmaydi
+     */
 
-import static com.itsuhbat.CRM.helper.Config.scanner;
-
-public class Service {
-    private Market market;
-
-    public Service(){
-
+    /** interfeys ichida kod yozish mumkin (Java 8 dan keyin qoshilgan)
+     * kod yozish uchun
+     * defoult va
+     * private keywordlaridan foydalanamiz
+     */
+    default void defoultMethod(){
+        System.out.println("this is defoult method");
     }
-    public Service(Market market) {
-        this.market = market;
-        initProduct();
+
+    private void privateMethod(){
+        System.out.println("this is private method");
     }
-     public void start(){
-        while (true){
-            System.out.println("Qanday foydalanuvchisiz? \n[1].Haridor \n[2].Sotuvchi \n[0].Dasturni to'xtatish");
-            int result = scanner.nextInt();
-            if (result == 1){
-                BuyerService buyer = new BuyerService(market);
-                buyer.start();break;
 
-            }else if (result == 2){
-                SellerService seller = new SellerService(market);
-                seller.start();
-                break;
-            }else if (result == 0){
-                System.exit(0);
-                break;
-            }
-            System.out.println("Noto'g'ri ma'lumot kiritildi!");
-        }
-     }
 
-     public void initProduct(){
-         Product[] products = new Product[]{
-                 new Product("olma", 4000d, "meva", 14d, "kg"),
-                 new Product("anor", 3000d, "meva", 5d, "kg"),
-                 new Product("nok", 4000d, "meva", 6d, "kg"),
-                 new Product("gosht", 7000d, "gosht", 14d, "kg"),
-                 new Product("shokolat", 5000d, "shirimlik", 54d, "kg"),
-                 new Product("banan", 6000d, "meva", 43d, "kg"),
-                 new Product("apelsin", 1000d, "meva", 64d, "kg"),
-         };
-         market.setProducts(products);
-     }
+    void start();
 
+    void initProduct();
 }
